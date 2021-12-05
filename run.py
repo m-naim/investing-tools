d987170ab39884c1f755df29ed70e92561f218a9
m-naim <mehenaouin@gmail.com>
2021-12-05 15:34:53 +0100

    server update

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from server import app
 
 if __name__ == '__main__':
     # Running app in debug mode
-    app.run(debug=True)
+    app.run(debug=True,use_reloader=True, port=5000, threaded=True)
--- server/__init__.py
+++ server/__init__.py
@@ -1,6 +1,18 @@
-from flask import Flask
+import os
+from flask import Flask,send_from_directory
 
 # Place where app is defined
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../client/build')
+
+# Serve React App
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    print('ehh')
+    print(path)
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 from server import stocks, predictions, portfolio
