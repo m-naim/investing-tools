ff237782c57149db79e7364778ba66ffac462179
m-naim <mehenaouin@gmail.com>
2022-08-23 23:10:50 +0200

    cors

--- server/__init__.py
+++ server/__init__.py
@@ -1,9 +1,11 @@
 
 import os
 from flask import Flask,send_from_directory
+from flask_cors import CORS
 
 # Place where app is defined
 app = Flask(__name__, static_folder='../client/build')
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 # Serve React App
 @app.route('/', defaults={'path': ''})
